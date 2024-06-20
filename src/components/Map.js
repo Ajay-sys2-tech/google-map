feafbd52eaebda67d43db5ed0722af3da4bde48c
ajay4985622 <ajay@vitto.money>
2024-06-20 22:57:36 +0530

    fixed the overflow issue

--- src/components/Map.js
+++ src/components/Map.js
@@ -84,13 +84,6 @@ export default function Map() {
       interactive: false
     });
 
-
-    // map.current.on('move', () => {
-    //   setLng(map.current.getCenter().lng.toFixed(4));
-    //   setLat(map.current.getCenter().lat.toFixed(4));
-    //   setZoom(map.current.getZoom().toFixed(2));
-    // });
-
     creteZoomControl(map);
 
   },[]);
